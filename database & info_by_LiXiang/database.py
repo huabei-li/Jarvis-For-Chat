d57a6e55cf94145031275988f0b291d4bd97577a
huabei-li <lixiang71908@hotmail.com>
2018-11-26 16:49:29 +0800

    新增处理用户状态的函数
    查询用户状态，修改状态为已登录、未登录

--- database & info_by_LiXiang/database.py
+++ database & info_by_LiXiang/database.py
@@ -74,8 +74,67 @@ def Insert_User(userID,nickname,tel,password):
         return sg
     return sg
 
+def getUserLoginStatus(userid):
+    '''
+    获取用户的登录状态
+    :param userid: 用户ID
+    :return: 用户登录状态，'1'为已登录，'0'为未登录
+    '''
+    import sqlite3
+    con = sqlite3.connect('Jarvis-forChat.db')
+    c = con.cursor()
+    cursor = c.execute("select log_status from user where userID= ? ", (userid,))
+    r = c.fetchall()
+    status = r[0][0]
+    return status
+
+def statusChange_Login(userid):
+    '''
+    将用户状态修改为已登录
+    :param userid: 用户ID
+    :return: 成功返回True，失败为False
+    '''
+    import sqlite3
+    sg = True
+    con = sqlite3.connect('Jarvis-forChat.db')
+    c = con.cursor()
+    try:
+        c.execute("update user set log_status ='1' where userID= ? ", (userid,))
+        con.commit()
+        con.close()
+    except Exception:
+        con.rollback()
+        sg = False
+        con.close()
+    finally:
+        return sg
+    return sg
+
+def statusChange_Logout(userid):
+    '''
+    将用户状态修改为未登录
+    :param userid: 用户ID
+    :return: 成功返回True，失败为False
+    '''
+    import sqlite3
+    sg = True
+    con = sqlite3.connect('Jarvis-forChat.db')
+    c = con.cursor()
+    try:
+        c.execute("update user set log_status ='0' where userID= ? ", (userid,))
+        con.commit()
+        con.close()
+    except Exception:
+        con.rollback()
+        sg = False
+        con.close()
+    finally:
+        return sg
+    return sg
+
 def getUserKeyword(userid, wxid):
     '''
+    获取用户设置的关键词列表
     :param userid:用户ID
     :param wxid:微信ID
     :return:用户设置关键词列表
